63de76188e0c69f3524ff42fec53951d950d2482
wanja727 <wanja727@naver.com>
2023-04-08 00:27:48 +0900

    first commit

---
+++ src/main/java/com/wanja727/kgjguide/KgjGuideApplication.java
@@ -0,0 +1,12 @@
+package com.wanja727.kgjguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgjGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgjGuideApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wanja727/kgjguide/KgjGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanja727.kgjguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgjGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
