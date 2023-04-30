a7b0e45af09f7dcdbb90a44b2e7e1fe6a8fbf42c
wanja727 <wanja727@naver.com>
2023-04-30 16:06:38 +0900

    oauth연계 관련 초기값 추가

--- src/main/java/com/wanja727/kgjguide/repository/UsersRepository.java
+++ src/main/java/com/wanja727/kgjguide/repository/UsersRepository.java
@@ -3,5 +3,9 @@ package com.wanja727.kgjguide.repository;
 import com.wanja727.kgjguide.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users,Long> {
+
+    Optional<Users> findByEmail(String email);
 }
